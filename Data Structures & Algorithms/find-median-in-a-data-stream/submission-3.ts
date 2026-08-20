7c19af9795934ef306d4ab8a0c074e78458b5ddc
Akos Osvald <elettartam@gmail.com>
2026-08-20 17:52:13 +0200

    Add: find-median-in-a-data-stream - submission-3

---
+++ Data Structures & Algorithms/find-median-in-a-data-stream/submission-3.ts
@@ -0,0 +1,40 @@
+class MedianFinder {
+    private small;
+    private large;
+    constructor() {
+        this.small = new PriorityQueue((a, b) => b - a);
+        this.large = new PriorityQueue((a, b) => a - b);
+    }
+
+    /**
+     *
+     * @param {number} num
+     * @return {void}
+     */
+    addNum(num: number): void {
+        if (this.large.isEmpty() || num > this.large.front()) {
+            this.large.enqueue(num);
+        } else {
+            this.small.enqueue(num);
+        }
+
+        if (this.small.size() > this.large.size() + 1) {
+            this.large.enqueue(this.small.dequeue());
+        } else if (this.large.size() > this.small.size() + 1) {
+            this.small.enqueue(this.large.dequeue());
+        }
+    }
+
+    /**
+     * @return {number}
+     */
+    findMedian(): number {
+        if (this.small.size() > this.large.size()) {
+            return this.small.front();
+        } else if (this.large.size() > this.small.size()) {
+            return this.large.front();
+        } else {
+            return (this.small.front() + this.large.front()) / 2.0;
+        }
+    }
+}

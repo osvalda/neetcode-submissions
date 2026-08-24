39a6d059c17fd08650cf115f50b4e01332c0c9be
Akos Osvald <elettartam@gmail.com>
2026-08-24 14:40:28 +0200

    Add: meeting-schedule - submission-0

---
+++ Data Structures & Algorithms/meeting-schedule/submission-0.ts
@@ -0,0 +1,29 @@
+/**
+ * Definition of Interval:
+ * class Interval  {
+ *   constructor(start, end) {
+ *     this.start = start;
+ *     this.end = end;
+ *   }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {Interval[]} intervals
+     * @returns {boolean}
+     */
+    canAttendMeetings(intervals: Interval[]): boolean {
+        let result = true;
+
+        intervals.sort((a, b) => a.start - b.start);
+
+        for (let i = 0; i < intervals.length - 1; i++) {
+            if (intervals[i].end > intervals[i + 1].start) {
+                return false;
+            }
+        }
+
+        return result;
+    }
+}

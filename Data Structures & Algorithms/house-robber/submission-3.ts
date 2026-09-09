d9883f4096fbcbf4532ca5703955a95c9dc0c7df
Akos Osvald <elettartam@gmail.com>
2026-09-09 16:16:08 +0200

    Add: house-robber - submission-3

---
+++ Data Structures & Algorithms/house-robber/submission-3.ts
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    rob(nums: number[]): number {
+        let rob1 = 0;
+        let rob2 = 0;
+
+        for (const num of nums) {
+            const temp = Math.max(num + rob1, rob2);
+            rob1 = rob2;
+            rob2 = temp;
+        }
+        return rob2;
+    }
+}

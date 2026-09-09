ad605a0ce54a6d87b5ca19eebe585a9b02597d19
Akos Osvald <elettartam@gmail.com>
2026-09-09 16:09:19 +0200

    Add: house-robber - submission-2

---
+++ Data Structures & Algorithms/house-robber/submission-2.ts
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    rob(nums: number[]): number {
+        if (nums.length === 0) return 0;
+        if (nums.length === 1) return nums[0];
+
+        const memo = new Int32Array(nums.length).fill(-1);
+        const dfs = (i) => {
+            if (i >= nums.length) {
+                return 0;
+            }
+            if (memo[i] !== -1) {
+                return memo[i];
+            }
+            return (memo[i] = Math.max(dfs(i + 1), nums[i] + dfs(i + 2)));
+        };
+        return dfs(0);
+    }
+}
